8409e80747af33100d01a1c4b8bd0a97e82dbed6
Christopher Goodwin <cgoodwin@Christophers-MacBook-Pro.local>
2019-11-23 13:26:10 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from PyPDF4 import PdfFileMerger
+import os
+from PyPDF4 import PdfFileReader
+from PyPDF4 import PdfFileWriter
+
+
+def merge_pdfs(file_paths, output='merged.pdf'):
+
+    pdf_merger = PdfFileMerger(open(output, "wb"))
+
+    for i in range(len(file_paths)):
+        pdf_merger.append(file_paths[i])
+
+    pdf_merger.write(output)
+
+    print(f"Output successfully written to {output}")
+    pdf_merger.close()
+
+
+if __name__ == '__main__':
+
+    # List comprehension, get list of every file in directory and add the absolute
+    # path to the file_paths list if '.pdf' is in the file name. Then sorts list
+    # alphabetically
+    file_paths = sorted([os.path.abspath(file) for file in os.listdir() if '.pdf' in file])
+
+
+    #merge_pdfs(file_paths)
+
+    print(file_paths)
+
